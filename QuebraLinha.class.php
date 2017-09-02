e20b9ae50210f8d360b267d8e8840c46cccc4ede
Gabriel Ribeiro <mazurco066@gmail.com>
2017-09-02 12:36:01 -0300

    Otimizando a Função

--- QuebraLinha.class.php
+++ QuebraLinha.class.php
@@ -1,10 +1,8 @@
 <?php
-
       /**
         * @author Mazurco066
         */
       class QuebraLinha implements TextWrapExerciseInterface {
-
         /**
           * @param string $text
           *   O texto que será utilizado como entrada.
@@ -16,13 +14,13 @@
           *   respeitando o comprimento de linha e as regras especificadas acima.
           */
         public static function textWrap(string $text, int $length): array{
-
           //Definindo Variáveis de uso geral
           $limiteLinha = 0; //Contador que armazena a posicao atual da linha que está
           $palavraGrande = 0; //Armazena o tamanho de uma palavra grande quando há uma
           $lin = 0; //Contador para o vetor de string de retorno
           $tamanhoArray = 0;  //Vai definir o tamanho do array de retorno
-          $retorno = array();
+          $retorno = array(); //Array de retorno da função
+          $retorno[$lin] = ""; //Inicializando oarray para não dar erros futuramente
 
           $text = explode(" ", $text);  //Método que divide uma string bruta palavra por palavra
           foreach($text as $key => $value) $text[$key] = $value." "; //Adicionando novamente os espaços removidos pelo médoto explode
@@ -31,105 +29,93 @@
 
             $lenghtPalavra = $limiteLinha + strlen($palavra) - 1; //Recebe a posicao quea palavra ocuparia
 
-            switch ($lenghtPalavra) {
+            if ($lenghtPalavra < $length){
+              //Se a palavra caber tranquilamente na linha
+              $retorno[$lin] .= $palavra; //Adiciona ao vetor a palavra
+              $limiteLinha += strlen($palavra); //Incrementa o tamanho da palavra ao contador
 
-              case ($lenghtPalavra < $length): //Se a palavra se encaixa dentro do limite definido
+            }
+            else if($lenghtPalavra == $length){
+              //Se a palavra caber apertadamente na linha
+              $retorno[$lin] .= $palavra;
+              $lin++;
+              $retorno[$lin] = "";
+              $limiteLinha = 0; //Zera o contador pois ja deu o limite dessa linha
 
-                  $retorno[$lin] .= $palavra;
+            }
+            else if($lenghtPalavra > $length){
+              //Se a palavra não caber na linha
+              if (strlen($palavra) - 1 > $length){  //Se for grande o suficiente a ponto de necessitar mais de 1 linha
 
-                $limiteLinha += strlen($palavra); //Incrementa o tamanho da palavra ao contador
-              break;
+                $palavraGrande = (strlen($palavra) - 1) / $length; //Calcula o tamanho que a palavra ira ocupar
 
-              case ($lenghtPalavra == $length):  //Se a palavra se encaixa ainda dentro do limite mais exatamente dentro do limite
-                $retorno[$lin] .= $palavra;
-                $lin++;
-                $retorno[$lin] = "";
-                $limiteLinha = 0; //Zera o contador pois ja deu o limite dessa linha
-              break;
+                for ($i = 0; $i <= $palavraGrande; $i++){ //Percorre a palavra grande caracter por caracter
 
-              case ($lenghtPalavra > $length):   //Se a palavra for grande e ultrapassar o limite
+                  $lengthPalavraGrande = strlen(substr($palavra, ($length * $i) - $limiteLinha, $length - 1));
+                  $caracteresDisponiveis = $length - $limiteLinha;  //Armazena os caracteres disponiveis
+                  $linha = substr($palavra, ($length * $i), $length); //Conteúdo que encaixara na linha
 
-                if (strlen($palavra) - 1 > $length){  //Se for grande o suficiente a ponto de necessitar mais de 1 linha
+                  if ($i == $palavraGrande ){ //Se chegar na ultima posição
 
-                  $palavraGrande = (strlen($palavra) - 1) / $length; //Calcula o tamanho que a palavra ira ocupar
+                    if ($lengthPalavraGrande > $caracteresDisponiveis){
 
-                  for ($i = 0; $i <= $palavraGrande; $i++){ //Percorre a palavra grande caracter por caracter
-
-                    $lengthPalavraGrande = strlen(substr($palavra, ($length * $i) - $limiteLinha, $length - 1));
-                    $caracteresDisponiveis = $length - $limiteLinha;  //Armazena os caracteres disponiveis
-                    $linha = substr($palavra, ($length * $i), $length); //Conteúdo que encaixara na linha
-
-                    if ($i == $palavraGrande ){ //Se chegar na ultima posição
-
-                        switch ($lengthPalavraGrande){
-
-                          case ($lengthPalavraGrande > $caracteresDisponiveis):
-                            //Se necessitar de mais uma linha
-                            $limiteLinha = strlen(substr($palavra, ($length * $i), $length)); //Quebra a linha na posição que parou
-                            $retorno[$lin] .= $linha;
-                            $lin++;
-                            $retorno[$lin] = "";
-                          break;
-
-                          case ($lengthPalavraGrande < $caracteresDisponiveis):
-                            //Se encaixar na linha
-                            $limiteLinha = strlen(substr($palavra, ($length * $i) - $limiteLinha, $length)); //Define a posição d alinha que parou e prossegue para proxima palavra
-                            $retorno[$lin] .= $linha;
-                          break;
-
-                          case ($lengthPalavraGrande == $caracteresDisponiveis):
-                            //Se encaixar exatamente no limite da linha
-                            $retorno[$lin] .= $linha;
-                            $lin++;
-                            $retorno[$lin] = "";
-                            $limiteLinha = 0; //Zera o contador
-                          break;
-                        }
+                      //Se necessitar de mais uma linha
+                      $limiteLinha = strlen(substr($palavra, ($length * $i), $length)); //Quebra a linha na posição que parou
+                      $retorno[$lin] .= $linha;
+                      $lin++;
+                      $retorno[$lin] = "";
 
                     }
-                    else{
+                    else if($lengthPalavraGrande < $caracteresDisponiveis){
 
-                      if ($limiteLinha - 1 != 0 || $limiteLinha - 1 != -1){
-                        //Se tiver alguma palavra na linha
-                        $lin++;
-                        $retorno[$lin] = "";
-                        $retorno[$lin] .= $linha;
-                        $limiteLinha = strlen(substr($palavra, ($length * $i) , $length));  //Retoma o contaddor
-                      }
-                      else{
-                        //Se estiver vazia
-                        $retorno[$lin] .= $linha;
-                        $lin++;
-                        $retorno[$lin] = "";
-                        $limiteLinha = 0; //Zera o contador
-                      }
+                      //Se encaixar na linha
+                      $limiteLinha = strlen(substr($palavra, ($length * $i) - $limiteLinha, $length)); //Define a posição d alinha que parou e prossegue para proxima palavra
+                      $retorno[$lin] .= $linha;
+
+                    }
+                    else if ($lengthPalavraGrande == $caracteresDisponiveis){
+
+                      //Se encaixar exatamente no limite da linha
+                      $retorno[$lin] .= $linha;
+                      $lin++;
+                      $retorno[$lin] = "";
+                      $limiteLinha = 0; //Zera o contador
 
                     }
 
                   }
+                  else{
 
+                    if ($limiteLinha - 1 != 0 || $limiteLinha - 1 != -1){
+                      //Se tiver alguma palavra na linha
+                      $lin++;
+                      $retorno[$lin] = "";
+                      $retorno[$lin] .= $linha;
+                      $limiteLinha = strlen(substr($palavra, ($length * $i) , $length));  //Retoma o contaddor
+                    }
+                    else{
+                      //Se estiver vazia
+                      $retorno[$lin] .= $linha;
+                      $lin++;
+                      $retorno[$lin] = "";
+                      $limiteLinha = 0; //Zera o contador
+                    }
+                  }
                 }
-                else{ //Se for grande e caber somente em uma linha
+              }
+              else{ //Se for grande e caber somente em uma linha
+                $lin++;
+                $retorno[$lin] = "";
+                $retorno[$lin] .= $palavra;
+                $limiteLinha = strlen($palavra);  //Incrementa o tamanho da palavra no contador
+              }
 
-                  $lin++;
-                  $retorno[$lin] = "";
-                  $retorno[$lin] .= $palavra;
-                  $limiteLinha = strlen($palavra);  //Incrementa o tamanho da palavra no contador
-                }
-              break;
-
-              default:
-                //Nada Acontece
-              break;
             }
 
           }
 
-          //print_r($retorno);
-
           return $retorno;
         }
-
         /**
           * Imprime na tela o texto formatado recebe como parametro o vetor de palavras
           * que foi formatado pela função textWrap
@@ -138,12 +124,10 @@
           *   O texto ja formatado pela função textWrap
           */
         public static function showText(array $text){
-
           foreach ($text as $key => $value) {
             print($value);  //Para cada elemento do vetor imprime-o na tela
             echo "<br>";
           }
-
         }
       }
  ?>
