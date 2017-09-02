3962607bbe8963c59dc13fb1b498fcb98ae6d589
Gabriel ribeiro <mazurco066@gmail.com>
2017-09-02 02:32:49 -0300

    Adição de mais alguns testes

--- test/CasoTeste.php
+++ test/CasoTeste.php
@@ -8,9 +8,39 @@
     function test01(){
       //Confirma se o retorno é um array
       $q = new QuebraLinha();
-      $retornoTeste =  $q->textWrap("Isso é um teste", 5);
+      $textoBruto = "essaPalavraéMuitoMuitoGrande = Texto Digitado por Gabriel Mazurco Ribeiro";
+      $retornoTeste =  $q->textWrap($textoBruto, 20);
       $this->assertInternalType('array',$retornoTeste);
     }
+
+    function test02(){
+      //Confirma se o retorno é um array denovo mas dessa vez diminuindo tamanho linear para 5
+      $q = new QuebraLinha();
+      $textoBruto = "essaPalavraéMuitoMuitoGrande = Texto Digitado por Gabriel Mazurco Ribeiro";
+      $retornoTeste =  $q->textWrap($textoBruto, 5);
+      $this->assertInternalType('array',$retornoTeste);
+    }
+
+    function test03(){
+      //Verifica se todos elementos do vetor de retorno são strings
+      $q = new QuebraLinha();
+      $textoBruto = "essaPalavraéMuitoMuitoGrande = Texto Digitado por Gabriel Mazurco Ribeiro";
+      $retornoTeste =  $q->textWrap($textoBruto, 5);
+      foreach ($retornoTeste as $key => $value) {
+        $this->assertInternalType('string',$value);
+      }
+    }
+
+    function test04(){
+      //Verifica se a string de uma linha contem pelo menos 1 ou mais caracteres
+      $q = new QuebraLinha();
+      $textoBruto = "essaPalavraéMuitoMuitoGrande = Texto Digitado por Gabriel Mazurco Ribeiro";
+      $retornoTeste =  $q->textWrap($textoBruto, 5);
+      foreach ($retornoTeste as $key => $value) {
+        $this->assertStringMatchesFormat('%s', $value);
+      }
+    }
+
 
   }
 
