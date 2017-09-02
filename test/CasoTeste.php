14b407b2d5eba30b5195600db571c04a2d95f33c
Gabriel ribeiro <mazurco066@gmail.com>
2017-09-02 13:33:37 -0300

    Testes Unitários

--- test/CasoTeste.php
+++ test/CasoTeste.php
@@ -5,6 +5,7 @@
 
   class QuebraLinhaTest extends PHPUnit\Framework\TestCase {
 
+    //TESTES DE VERIFICAÇÃO DE TIPO DE RETORNO
     function test01(){
       //Confirma se o retorno é um array
       $q = new QuebraLinha();
@@ -31,7 +32,16 @@
       }
     }
 
+    //TESTES DE VALOR LIMITE INFERIOR (NO CASO SÓ VAI TER INFERIOR MSM POR QUE NÃO É DEFINIDO NOS REQUISITOS VALOR LIMITE MAXIMO)
     function test04(){
+      //Verifica se Retorna valor inválido com tamanho inválido
+      $q = new QuebraLinha();
+      $textoBruto = "Texto Digitado por Gabriel Mazurco Ribeiro";
+      $retornoTeste =  $q->textWrap($textoBruto, 0);
+      $this->assertEquals("Por favor entre com algum limite válido", $retornoTeste[0]);
+    }
+
+    function test05(){
       //Verifica se todos retornos sao strings com teste de limite inferior
       $q = new QuebraLinha();
       $textoBruto = "Texto Digitado por Gabriel Mazurco Ribeiro";
@@ -41,15 +51,18 @@
       }
     }
 
-    function test05(){
-      //Verifica se Retorna valor inválido com tamanho inválido
+    function test06(){
+      //Verifica se formato do retorno esta como especificado nos requisitos mas com analise de valor limite e palavra grande
       $q = new QuebraLinha();
-      $textoBruto = "Texto Digitado por Gabriel Mazurco Ribeiro";
-      $retornoTeste =  $q->textWrap($textoBruto, 0);
-      $this->assertEquals("Por favor entre com algum limite válido", $retornoTeste[0]);
+      $textoBruto = "Isso e um teste";
+      $retornoTeste =  $q->textWrap($textoBruto, 2);
+      //'Isso' palavra grande demais entao pula uma linha e começa com metade dela e termina na segunda linha
+      //O mesmo vale para a palavra Teste
+      $this->assertEquals(array('', 'Is', 'so ', 'e ', 'um ', '', 'te', 'st', 'e '), $retornoTeste);
     }
 
-    function test06(){
+    //FINALMENTE O TESTE PARA VALIDAR SE O RETORNO ESTA DE ACORDO COM O REQUISITO ESPECIFICADO
+    function test07(){
       //Verifica se formato do retorno esta como especificado nos requisitos
       $q = new QuebraLinha();
       $textoBruto = "Isso e um teste";
