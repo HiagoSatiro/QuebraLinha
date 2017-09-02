e5c4484546fd31bf4fca05a3acbba8f7461a5a6b
Gabriel Ribeiro <mazurco066@gmail.com>
2017-09-02 16:52:22 -0300

    Testes Unitários

--- test/CasoTeste.php
+++ test/CasoTeste.php
@@ -32,8 +32,18 @@
       }
     }
 
-    //TESTES DE VALOR LIMITE INFERIOR (NO CASO SÓ VAI TER INFERIOR MSM POR QUE NÃO É DEFINIDO NOS REQUISITOS VALOR LIMITE MAXIMO)
     function test04(){
+      //Verifica se todos elementos do vetor de retorno são strings com um texto diferente e muito longo
+      $q = new QuebraLinha();
+      $textoBruto = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eget enim at mi fermentum malesuada. Sed a nisi vitae quam sollicitudin eleifend in vel quam. Sed lobortis ut nibh vel lacinia. Duis nunc velit, placerat in tincidunt quis, pharetra at odio. Donec iaculis ultrices ultrices. Ut dictum arcu nisl, a vestibulum augue pharetra eu. Integer semper lorem sed elit laoreet egestas. Praesent scelerisque tempor lacus. Etiam mollis orci sapien, et scelerisque neque sodales a. Morbi imperdiet sed nunc nec aliquet. Proin semper, ex ut semper pretium, elit tellus consectetur orci, sed imperdiet tortor neque viverra diam. Pellentesque ex augue, mattis vitae fermentum in, euismod vel metus. Integer et pellentesque lacus. Mauris lacinia purus ut lacinia eleifend.";
+      $retornoTeste =  $q->textWrap($textoBruto, 20);
+      foreach ($retornoTeste as $key => $value) {
+        $this->assertInternalType('string',$value);
+      }
+    }
+
+    //TESTES DE VALOR LIMITE INFERIOR (NO CASO SÓ VAI TER INFERIOR MSM POR QUE NÃO É DEFINIDO NOS REQUISITOS VALOR LIMITE MAXIMO)
+    function test05(){
       //Verifica se Retorna valor inválido com tamanho inválido
       $q = new QuebraLinha();
       $textoBruto = "Texto Digitado por Gabriel Mazurco Ribeiro";
@@ -41,7 +51,7 @@
       $this->assertEquals("Por favor entre com algum limite válido", $retornoTeste[0]);
     }
 
-    function test05(){
+    function test06(){
       //Verifica se todos retornos sao strings com teste de limite inferior
       $q = new QuebraLinha();
       $textoBruto = "Texto Digitado por Gabriel Mazurco Ribeiro";
@@ -51,7 +61,7 @@
       }
     }
 
-    function test06(){
+    function test07(){
       //Verifica se formato do retorno esta como especificado nos requisitos mas com analise de valor limite e palavra grande
       $q = new QuebraLinha();
       $textoBruto = "Isso e um teste";
@@ -62,7 +72,7 @@
     }
 
     //FINALMENTE O TESTE PARA VALIDAR SE O RETORNO ESTA DE ACORDO COM O REQUISITO ESPECIFICADO
-    function test07(){
+    function test08(){
       //Verifica se formato do retorno esta como especificado nos requisitos
       $q = new QuebraLinha();
       $textoBruto = "Isso e um teste";
